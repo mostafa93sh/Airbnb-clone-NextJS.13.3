3117e33d0a6be6fb89258007a8c760d3b6428324
mostafa93sh <mostafa.khalel.93@gmail>
2023-05-09 13:08:20 +0200

    init start project with navbar created

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import NavBar from "./components/NavBar/NavBar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb Project",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
