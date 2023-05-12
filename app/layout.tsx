974b514257d0b34e43db549011bd11f80a2f2416
mostafa93sh <mostafa.khalel.93@gmail>
2023-05-12 14:37:28 +0300

    add custom hook for showing login modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        {/* <ToasterProvider /> */}
+        <ToasterProvider />
         <RegisterModal />
         <NavBar />
         {children}
